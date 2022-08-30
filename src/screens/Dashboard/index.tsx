8a75108f56557df81e197253936b6d2c02dd7317
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-08-30 20:21:36 -0300

    refactor HighlightCard component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -13,6 +13,50 @@ import {
 } from "./styles";
 import { HighlightCard } from "../../components/HighlightCard";
 
+interface IHighlightCardContent {
+  type: "up" | "down" | "total";
+  title: string;
+  amount: string;
+  lastTransaction: string;
+}
+
+const HighlightCardContent: IHighlightCardContent[] = [
+  {
+    type: "up",
+    title: "Entradas",
+    amount: 'R$ 17.400,00"',
+    lastTransaction: "Última entrada dia 13 de abril",
+  },
+  {
+    type: "down",
+    title: "Saídas",
+    amount: "R$ 1.259,00",
+    lastTransaction: "Última saída dia 03 de abril",
+  },
+  {
+    type: "total",
+    title: "Total",
+    amount: "R$ 16.141,00",
+    lastTransaction: "01 à 16 de abril",
+  },
+];
+
+const renderHighlightCard = ({
+  type,
+  title,
+  amount,
+  lastTransaction,
+}: IHighlightCardContent): JSX.Element => {
+  return (
+    <HighlightCard
+      type={type}
+      title={title}
+      amount={amount}
+      lastTransaction={lastTransaction}
+    />
+  );
+};
+
 export function Dashboard() {
   return (
     <Container>
@@ -33,26 +77,8 @@ export function Dashboard() {
           <IconFeather name="power" />
         </UserWrapper>
       </Header>
-
       <HighlightCards>
-        <HighlightCard
-          type="up"
-          title="Entradas"
-          amount="R$ 17.400,00"
-          lastTransaction="Última entrada dia 13 de abril"
-        />
-        <HighlightCard
-          type="down"
-          title="Saídas"
-          amount="R$ 1.259,00"
-          lastTransaction="Última saída dia 03 de abril"
-        />
-        <HighlightCard
-          type="total"
-          title="Total"
-          amount="R$ 16.141,00"
-          lastTransaction="01 à 16 de abril"
-        />
+        {HighlightCardContent.map(renderHighlightCard)}
       </HighlightCards>
     </Container>
   );
