f11ab50cc8565925b2fe0a2508c62763c1b2d8c5
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-08-30 00:17:57 -0300

    add Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Container, Title } from './styles'
+import React from "react";
+import {
+  Container,
+  Header,
+  UserContainer,
+  UserPhoto,
+  UserTextWrapper,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  IconFeather,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserContainer>
+            <UserPhoto
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/73440184?v=4",
+              }}
+            />
+            <UserTextWrapper>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Anderson</UserName>
+            </UserTextWrapper>
+          </UserContainer>
+
+          <IconFeather name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
-  )
+  );
 }
